9092ccb8af90d48f339222d72f2d906f72887119
Unknown <benybudi.bb@gmail.com>
2018-01-09 22:25:18 +0700

    Final Fix

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var currMenu = "home";
 
 var names = [];
@@ -54,12 +53,13 @@ function prevPage(){
 			CurrIndex = i;
 		}
 	}
-	NxtIndex = CurrIndex+1;
+	NxtIndex = CurrIndex-1;
+	NxtIndex %= items.length;	
 
 	var curr = document.getElementsByClassName(currMenu)[0];
-	var nxt = document.getElementsByClassName(names[(CurrIndex-1)%items.length])[0];
+	var nxt = document.getElementsByClassName(names[NxtIndex])[0];
 
-	doChange(curr, nxt, CurrIndex, NxtIndex, names[(CurrIndex-1)%items.length]);
+	doChange(curr, nxt, CurrIndex, NxtIndex, names[NxtIndex]);
 }
 
 
@@ -98,104 +98,4 @@ function doChange(curr, nxt, CurrIndex, NxtIndex, menuName){
 	}, 500);
 
 	currMenu = menuName;
-=======
-var currMenu = "home";
-
-var names = [];
-var items = [];
-
-function init(){
-	items = document.getElementsByClassName('nav-link');
-	
-	for(var i = 0; i<items.length; i++){
-		names[i] = items[i].name;
-		console.log(names[i]);
-	}
-
-	items[0].addEventListener('click', function(event){
-		changeMenu(names[0]);
-	});
-	items[1].addEventListener('click', function(event){
-		changeMenu(names[1]);
-	});
-	items[2].addEventListener('click', function(event){
-		changeMenu(names[2]);
-	});
-	items[3].addEventListener('click', function(event){
-		changeMenu(names[3]);
-	});
-	items[4].addEventListener('click', function(event){
-		changeMenu(names[4]);
-	});
-}
-
-function nextPage(){
-	var NxtIndex = 0;
-	var CurrIndex = 0;
-	for (var i = 0; i<names.length; i++){
-		if(currMenu == names[i]) {
-			CurrIndex = i;
-		}
-	}
-	NxtIndex = CurrIndex+1;
-
-	var curr = document.getElementsByClassName(currMenu)[0];
-	var nxt = document.getElementsByClassName(names[(CurrIndex+1)%items.length])[0];
-
-	doChange(curr, nxt, CurrIndex, NxtIndex, names[(CurrIndex+1)%items.length]);
-}
-
-function prevPage(){
-	var NxtIndex = 0;
-	var CurrIndex = 0;
-	for (var i = 0; i<names.length; i++){
-		if(currMenu == names[i]) {
-			CurrIndex = i;
-		}
-	}
-	NxtIndex = CurrIndex+1;
-
-	var curr = document.getElementsByClassName(currMenu)[0];
-	var nxt = document.getElementsByClassName(names[(CurrIndex-1)%items.length])[0];
-
-	doChange(curr, nxt, CurrIndex, NxtIndex, names[(CurrIndex-1)%items.length]);
-}
-
-
-function changeMenu(menuName){
-	console.log(menuName);
-	if (currMenu == menuName) return;
-
-
-	var NxtIndex = 0;
-	var CurrIndex = 0;
-
-	for (var i = 0; i<names.length; i++){
-		if(menuName == names[i]) {
-			NxtIndex = i;
-		}
-		if(currMenu == names[i]) {
-			CurrIndex = i;
-		}
-	}
-
-	var curr = document.getElementsByClassName(currMenu)[0];
-	var nxt = document.getElementsByClassName(menuName)[0];
-
-	doChange(curr, nxt, CurrIndex, NxtIndex, menuName);
-}
-
-function doChange(curr, nxt, CurrIndex, NxtIndex, menuName){
-	curr.classList.toggle('menu--off');
-	nxt.classList.toggle('menu--on');
-	curr.classList.remove('menu--on');
-	items[CurrIndex].classList.remove('active');
-	items[NxtIndex].classList.toggle('active');
-
-	window.setTimeout(function(){
-		curr.classList.remove('menu--off');
-	}, 500);
-
-	currMenu = menuName;
->>>>>>> 8519e1c6ee128eabd4813c84163f9ea3a67037f7
 }
