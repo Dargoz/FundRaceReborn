8fd662e5b57570433ee27156a552fbfbcdb5819e
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2018-01-21 13:25:48 +0700

    re:Design UI login & Regis + Add CSS for hidden in Comment

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,33 +1,38 @@
 
-var config = {
-	apiKey: "AIzaSyD_0MubOduGW0MGGtVn2HMeBN0e4vmc2vU",
-	authDomain: "fundrace-46c75.firebaseapp.com",
-	databaseURL: "https://fundrace-46c75.firebaseio.com",
-	projectId: "fundrace-46c75",
-	storageBucket: "fundrace-46c75.appspot.com",
-	messagingSenderId: "505786262262"
-};
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyD_0MubOduGW0MGGtVn2HMeBN0e4vmc2vU",
+    authDomain: "fundrace-46c75.firebaseapp.com",
+    databaseURL: "https://fundrace-46c75.firebaseio.com",
+    projectId: "fundrace-46c75",
+    storageBucket: "fundrace-46c75.appspot.com",
+    messagingSenderId: "505786262262"
+  };
+  firebase.initializeApp(config);
+
+
 
 //Get element
+const preObject = document.getElementById('users'); 
 const btnLogin = document.getElementById('btn-login');
 const getUsername = document.getElementById('login-username');
 const getPass = document.getElementById('login-pass');
 
 //Create-References
-const dbRefOject = firebase.database().ref().child('users').child('user1');
+const dbRefOject = firebase.database().ref().child('users');
 
 //firebase.auth().signOut();
 //Sync object changes
-dbRefOject.on('child_added', snap => console.log(snap.val()));
+
 
 var experiment ="";
-var usernameList= [];
-var passwordList= [];
+var userList = [];
+var usernameList = [];
+var emailList = [];
+var passwordList = [];
 
-firebase.database().ref('users').once('value', function(snapshot) {
+firebase.database().ref().child('users').once('value', function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
-    var childKey = childSnapshot.key;
+		var childKey = childSnapshot.key;
     // var childData = childSnapshot.val();
 		firebase.database().ref('/users/' + childKey).once('value', function(snapshot){
 			var readUsername = snapshot.child('username').val();
@@ -37,64 +42,70 @@ firebase.database().ref('users').once('value', function(snapshot) {
 			
 	 });
 	});
+	console.log("total user: " + usernameList.length);
 });
 
-function writeUserData(userId, userName, email, pass) {
+function writeUserData(userId, userName, pass) {
   firebase.database().ref('users/' + userId).set({
 		username: userName,
-		email: email,
     password: pass
   });
 }
 
+// firebase.auth().signInAnonymously().catch(function(error) {
+// 	// Handle Errors here.
+// 	var errorCode = error.code;
+// 	var errorMessage = error.message;
+// 	// ...
+// 	});
 //firebase.auth().signInWithEmailAndPassword("davinreinaldogozali@gmail.com","Tsuchikage123");
 //console.log("usernya :" + firebase.auth().currentUser.uid);
-const auth = firebase.auth();
+
 // const promise = firebase.auth().signInWithEmailAndPassword("davinreinaldogozali@gmail.com","Tsuchikage123");
 // console.log(promise);
 //auth.signOut();
-btnLogin.addEventListener('click', e =>{
-	const email = getUsername.value;
-	const pass = getPass.value;
-	
-	console.log(email);
-	console.log(pass);
-	//Sign in
-  auth.signInWithEmailAndPassword(email,pass);
-	
-	
-	// const promise = auth.signInWithEmailAndPassword(email,pass);
-	// promise.catch(e => console.log(e.message));
-});
 
-auth.onAuthStateChanged(user =>{
-	if(user){
-		console.log(user.uid);
-	}else{
-		console.log("not logged in");
-	}
-});
+// btnLogin.addEventListener('click', e => {
+// 	const email = getUsername.value;
+// 	const pass = getPass.value;
 
-if (firebase.auth().currentUser !== null) {
-				console.log("user id: " + firebase.auth().currentUser.uid);
-}
+// 	console.log(email);
+// 	console.log(pass);
+// 	//Sign in
+// 	result = auth.signInWithEmailAndPassword(email, pass);
+
+// 	// const promise = auth.signInWithEmailAndPassword(email,pass);
+// 	// promise.catch(e => console.log(e.message));
+// });
+
+
+// auth.onAuthStateChanged(user => {
+	
+// 	if (user) {
+// 		console.log(user.uid);
+// 	} else {
+// 		console.log("not logged in");
+// 	}
+// });
+
+
 function validate(){
+
 	console.log("Jalan");
-	
 	var tempUserName = document.getElementById('input-username').value;
 	var tempEmail = document.getElementById('input-email').value;
 	var tempPass = document.getElementById('input-password').value;
 	console.log(tempUserName);
 	console.log(tempEmail);
 	console.log(tempPass);
-	firebase.auth().createUserWithEmailAndPassword(tempEmail, tempPass).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
-	});
+	// firebase.auth().createUserWithEmailAndPassword(tempEmail, tempPass).catch(function(error) {
+	// 	// Handle Errors here.
+	// 	var errorCode = error.code;
+	// 	var errorMessage = error.message;
+	// 	// ...
+	// });
 
-	//writeUserData("user3",tempUserName,"coba");
+	writeUserData("user4",tempUserName,tempPass);
 
 }
 
@@ -116,23 +127,32 @@ function loginAuth(){
 			index = i;
 		}
 	}
-	firebase.auth().signInAnonymously().catch(function(error) {
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	// ...
-	});
+
+	const email = getUsername;
+	const pass = getPass;
+
+	console.log(email);
+	console.log(pass);
+	//Sign in
+	//auth.signInWithEmailAndPassword(email, pass);
+
+	// firebase.auth().signInAnonymously().catch(function(error) {
+	// // Handle Errors here.
+	// var errorCode = error.code;
+	// var errorMessage = error.message;
+	// // ...
+	// });
 	// firebase.auth().signOut().then(function() {
 	// 	// Sign-out successful.
 	// }).catch(function(error) {
 	// 	// An error happened.
 	// });
 
-
-	// 	alert("login success");
-	// }else{
-	// 	alert("login failed");
-	// }
+	if(check == true){
+		alert("login success");
+	}else{
+		alert("login failed");
+	}
 
 }
 
