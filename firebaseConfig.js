e9fc489163f15f20a6b2ba64805b88a5f53970a7
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2018-02-05 13:54:37 +0700

    added Chat System!

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -23,12 +23,27 @@ const dbRefOject = firebase.database().ref().child('users');
 //firebase.auth().signOut();
 //Sync object changes
 
-
+const messaging = firebase.messaging();
 var experiment ="";
 var userList = [];
 var usernameList = [];
 var emailList = [];
 var passwordList = [];
+
+messaging.requestPermission()
+.then(function() {
+  console.log('Notification permission granted.');
+  // TODO(developer): Retrieve an Instance ID token for use with FCM.
+  // ...
+})
+.catch(function(err) {
+  console.log('Unable to get permission to notify.', err);
+});
+
+  // Get Instance ID token. Initially this makes a network call, once retrieved
+  // subsequent calls to getToken will return from cache.
+
+
 
 firebase.database().ref().child('users').once('value', function(snapshot) {
   snapshot.forEach(function(childSnapshot) {
